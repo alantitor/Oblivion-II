9a4ff128d85adb2ea4cdc398296d374752543e45
alantitor <belosianau@gmail.com>
2015-06-26 01:00:41 +0800

    update

--- app/src/main/java/ntou/cs/lab505/oblivionii/activities/BandSettingActivity.java
+++ app/src/main/java/ntou/cs/lab505/oblivionii/activities/BandSettingActivity.java
@@ -1,9 +1,9 @@
 package ntou.cs.lab505.oblivionii.activities;
 
 import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -11,14 +11,22 @@ import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import ntou.cs.lab505.oblivionii.R;
+import ntou.cs.lab505.oblivionii.database.BandSettingAdapter;
+import ntou.cs.lab505.oblivionii.database.IOSettingAdapter;
+import ntou.cs.lab505.oblivionii.datastructure.BandGainSetUnit;
 
 public class BandSettingActivity extends Activity {
 
+    private int channelNumber;
+    private LinearLayout rightControlLayout;
     private LinearLayout leftRootLayout;
     private LinearLayout rightRootLayout;
     private SeekBar leftSeekbar;
     private SeekBar rightSeekBar;
+    private TextView leftControlTV;
     private TextView leftCountTV;
     private TextView rightCountTV;
     private int leftItemNumber;
@@ -30,10 +38,12 @@ public class BandSettingActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_band_setting);
 
-        leftCountTV = (TextView) findViewById(R.id.count_left_activity_band_setting);
-        rightCountTV = (TextView) findViewById(R.id.count_right_activity_band_setting);
+        rightControlLayout = (LinearLayout) findViewById(R.id.control_right_activity_band_setting);
         leftRootLayout = (LinearLayout) findViewById(R.id.drawarea_left_activity_band_setting);
         rightRootLayout = (LinearLayout) findViewById(R.id.drawarea_right_activity_band_setting);
+        leftControlTV = (TextView) findViewById(R.id.controltitle_left_activity_band_setting);
+        leftCountTV = (TextView) findViewById(R.id.count_left_activity_band_setting);
+        rightCountTV = (TextView) findViewById(R.id.count_right_activity_band_setting);
         leftSeekbar = (SeekBar) findViewById(R.id.seekbar_left_activity_band_setting);
         rightSeekBar = (SeekBar) findViewById(R.id.seekbar_right_activity_band_setting);
 
@@ -86,30 +96,125 @@ public class BandSettingActivity extends Activity {
 
             }
         });
+
+
+        // check channel number.
+        IOSettingAdapter ioSettingAdapter = new IOSettingAdapter(this.getApplicationContext());
+        ioSettingAdapter.open();
+        this.channelNumber = ioSettingAdapter.getData().getChannelNumber();
+        //Log.d("BandSettingActivity", "in onCreate. channel number: " + this.channelNumber);
+        ioSettingAdapter.close();
+
+        if (this.channelNumber == 1) {
+            leftControlTV.setText("聲道");
+            rightControlLayout.setVisibility(View.GONE);
+        }
     }
 
+    /**
+     *
+     */
     @Override
     public void onResume() {
         super.onResume();
 
-        LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // get data from database.
+        BandSettingAdapter bandSettingAdapter = new BandSettingAdapter(this.getApplicationContext());
+        bandSettingAdapter.open();
+        ArrayList<BandGainSetUnit> bandGainSetUnitArrayList = bandSettingAdapter.getData();
+        bandSettingAdapter.close();
+
+        // load data to layout.
 
 
+        Log.d("BandSettingActivity", "in onResume. size:" + bandGainSetUnitArrayList.size());
+
+        for (int count = 0; count < bandGainSetUnitArrayList.size(); count++) {
+            if (bandGainSetUnitArrayList.get(count).getLr() == 0) {
+
+            } else if (bandGainSetUnitArrayList.get(count).getLr() == 1) {
+
+            } else {
+                Log.d("BandSettingActivity", "in onResume. continue.");
+            }
+        }
+
+        // set seekBar progress.
 
     }
 
+    /**
+     *
+     */
     @Override
     public void onPause() {
         super.onPause();
 
         View v;
-        EditText lowBand, highband, gain40, gain60, gain80;
-        ContentValues insertValues;
+        EditText lowBand, highBand, gain40, gain60, gain80;
 
         // delete old data.
+        BandSettingAdapter bandSettingAdapter = new BandSettingAdapter(this.getApplicationContext());
+        bandSettingAdapter.open();
+        bandSettingAdapter.deleteData();
 
+
+        ArrayList<BandGainSetUnit> bandGainSetUnitArrayList = new ArrayList<>();
+        BandGainSetUnit bandGainSetUnit = null;
 
         // insert new data.
+        // process left channel data.
+        for (int count = 0; count < leftItemNumber; count++) {
+            // get values from view fields.
+            v = leftRootLayout.getChildAt(count);
+            lowBand = (EditText) v.findViewById(R.id.lowBand_view_bandgain);
+            highBand = (EditText) v.findViewById(R.id.highband_view_bandgain);
+            gain40 = (EditText) v.findViewById(R.id.gain40_view_bandgain);
+            gain60 = (EditText) v.findViewById(R.id.gain60_view_bandgain);
+            gain80 = (EditText) v.findViewById(R.id.gain80_view_bandgain);
+            //Log.d("BandSettingActivity", "in onPause. lowBand: " + lowBand.getText().toString());
+            //Log.d("BandSettingActivity", "in onPause. highBand: " + highBand.getText().toString());
+            //Log.d("BandSettingActivity", "in onPause. gain40Band: " + gain40.getText().toString());
+            //Log.d("BandSettingActivity", "in onPause. gain60Band: " + gain60.getText().toString());
+            //Log.d("BandSettingActivity", "in onPause. gain80Band: " + gain80.getText().toString());
 
+
+            // generate object to save values.
+            bandGainSetUnit = new  BandGainSetUnit(0,
+                                                    Integer.parseInt(lowBand.getText().toString()),
+                                                    Integer.parseInt(highBand.getText().toString()),
+                                                    Integer.parseInt(gain40.getText().toString()),
+                                                    Integer.parseInt(gain60.getText().toString()),
+                                                    Integer.parseInt(gain80.getText().toString()));
+            bandGainSetUnitArrayList.add(bandGainSetUnit);
+
+        }
+
+        // process right channel data.
+        if (this.channelNumber == 2) {
+            for (int count = 0; count < rightItemNumber; count ++) {
+                // get values from view fields.
+                v = rightRootLayout.getChildAt(count);
+                lowBand = (EditText) v.findViewById(R.id.lowBand_view_bandgain);
+                highBand = (EditText) v.findViewById(R.id.highband_view_bandgain);
+                gain40 = (EditText) v.findViewById(R.id.gain40_view_bandgain);
+                gain60 = (EditText) v.findViewById(R.id.gain60_view_bandgain);
+                gain80 = (EditText) v.findViewById(R.id.gain80_view_bandgain);
+
+                // generate object to save values.
+                bandGainSetUnit = new  BandGainSetUnit(1,
+                                                        Integer.parseInt(lowBand.getText().toString()),
+                                                        Integer.parseInt(highBand.getText().toString()),
+                                                        Integer.parseInt(gain40.getText().toString()),
+                                                        Integer.parseInt(gain60.getText().toString()),
+                                                        Integer.parseInt(gain80.getText().toString()));
+                bandGainSetUnitArrayList.add(bandGainSetUnit);
+            }
+        }
+
+        // save data to database.
+        bandSettingAdapter.saveData(bandGainSetUnitArrayList);
+        // close database
+        bandSettingAdapter.close();
     }
 }
--- app/src/main/java/ntou/cs/lab505/oblivionii/database/BandSettingAdapter.java
+++ app/src/main/java/ntou/cs/lab505/oblivionii/database/BandSettingAdapter.java
@@ -1,9 +1,16 @@
 package ntou.cs.lab505.oblivionii.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+
 import ntou.cs.lab505.oblivionii.database.helper.DBHelper;
+import ntou.cs.lab505.oblivionii.database.helper.DBParams;
+import ntou.cs.lab505.oblivionii.database.helper.TableContract;
+import ntou.cs.lab505.oblivionii.datastructure.BandGainSetUnit;
 
 /**
  * Created by alan on 6/22/15.
@@ -28,15 +35,76 @@ public class BandSettingAdapter {
         this.mDbHelper.close();
     }
 
-    public void saveData() {
+    public void saveData(ArrayList<BandGainSetUnit> dataUnit) {
 
+        String[] projection = {TableContract._ID,
+                                TableContract.T_BAND_USERID};
+        String selection = TableContract.T_BAND_USERID + " = ? ";
+        String[] selectionArgs = {DBParams.USER_ID};
+        String sortOrder = "";
+        Cursor c = mDb.query(TableContract.TABLE_BAND, projection, selection, selectionArgs, null, null, sortOrder);
+        c.moveToFirst();
+
+        if (c.getCount() != 0) {
+            deleteData();
+        } else {
+            // save valuesto database.
+            ContentValues insertValues = null;
+
+            for (int count = 0; count < dataUnit.size(); count++) {
+                insertValues = new ContentValues();
+                insertValues.put(TableContract.T_BAND_USERID, DBParams.USER_ID);
+                insertValues.put(TableContract.T_BAND_LR, dataUnit.get(count).getLr());
+                insertValues.put(TableContract.T_BAND_LOWBAND, dataUnit.get(count).getLowBand());
+                insertValues.put(TableContract.T_BAND_HIGHBAND, dataUnit.get(count).getHighBand());
+                insertValues.put(TableContract.T_BAND_GAIN40, dataUnit.get(count).getGain40());
+                insertValues.put(TableContract.T_BAND_GAIN60, dataUnit.get(count).getGain60());
+                insertValues.put(TableContract.T_BAND_GAIN80, dataUnit.get(count).getGain80());
+                insertValues.put(TableContract.T_BAND_STATE, "1");
+                mDb.insert(TableContract.TABLE_BAND, null, insertValues);
+            }
+        }
     }
 
-    public void getData() {
+    public ArrayList<BandGainSetUnit> getData() {
 
+        String[] projection = {TableContract.T_BAND_USERID,
+                                TableContract.T_BAND_LR,
+                                TableContract.T_BAND_LOWBAND,
+                                TableContract.T_BAND_HIGHBAND,
+                                TableContract.T_BAND_GAIN40,
+                                TableContract.T_BAND_GAIN60,
+                                TableContract.T_BAND_GAIN80,
+                                TableContract.T_BAND_STATE};
+        String selection = TableContract.T_BAND_USERID + " = ? ";
+        String[] selectionArgs = {DBParams.USER_ID};
+        String sortOrder = "";
+        Cursor c = mDb.query(TableContract.TABLE_BAND, projection, selection, selectionArgs, null, null, sortOrder);
+        c.moveToFirst();
+
+        ArrayList<BandGainSetUnit> bandGainSetUnitArrayList = new ArrayList<>();
+        BandGainSetUnit bandGainSetUnit = null;
+        int lr, lowBand, highBand, gain40, gain60, gain80;
+
+
+        while (!c.isAfterLast()) {
+            lr = Integer.parseInt(c.getString(c.getColumnIndex(TableContract.T_BAND_LR)));
+            lowBand = Integer.parseInt(c.getString(c.getColumnIndex(TableContract.T_BAND_LOWBAND)));
+            highBand = Integer.parseInt(c.getString(c.getColumnIndex(TableContract.T_BAND_HIGHBAND)));
+            gain40 = Integer.parseInt(c.getString(c.getColumnIndex(TableContract.T_BAND_GAIN40)));
+            gain60 = Integer.parseInt(c.getString(c.getColumnIndex(TableContract.T_BAND_GAIN60)));
+            gain80 = Integer.parseInt(c.getString(c.getColumnIndex(TableContract.T_BAND_GAIN80)));
+            bandGainSetUnit = new BandGainSetUnit(lr, lowBand, highBand, gain40, gain60, gain80);
+            bandGainSetUnitArrayList.add(bandGainSetUnit);
+
+            c.moveToNext();  // go to next.
+        }
+
+        return bandGainSetUnitArrayList;
     }
 
     public void deleteData() {
-
+        //Log.d("BandSettingAdapter", "in deleteData. delete data.");
+        mDb.delete(TableContract.TABLE_BAND, TableContract.T_BAND_USERID + " = " + DBParams.USER_ID, null);
     }
 }
